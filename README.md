70f0f4a68ae96db8a4f59755c2ab7d3a893619fd
sarikajadhav20 <sarikasj2004@gmail.com>
2025-09-10 18:19:45 +0530

    new changes added

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a simple full-stack (MERN) application to add, list and browse movies.
 
 >Note:This may take 1-2 minutes to load on a first visit since we are using free plan of onrender to host backend it goes to sleep after 15 mins of inactivity.
 
+Visit the [Live Demo](https://popcorn-db-frontend.netlify.app/) to see it in action!
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -8,6 +9,18 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
+const connectDB=async()=>{
+    try{
+    const conn=await mongoose.connect(process.env.MONGODB_URL);
+
+    if(conn){
+        console.log("MongoDB connected");
+    }
+}    catch(err){
+    console.error(err.message);
+}
+}
+ 
 app.get('/',(req,res)=>{
     res.json({status:"Ok" , message:"Server is healthy" });
 });
@@ -16,4 +29,5 @@ const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
